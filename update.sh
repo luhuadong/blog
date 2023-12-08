46c69367f1a615611d0528d7d3ab0c7ec7d47818
luhuadong <luhuadong@163.com>
2023-12-08 14:37:46 +0800

    sync 20231208

--- update.sh
+++ update.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-rm -rf docs
-cp -r public docs
+HTML_SRC_DIR=public
+HTML_SRC_DIR2=../blog/public
+TARGET_DIR=docs
+
+if [ -d ${HTML_SRC_DIR} ]; then
+    echo "Copy ${HTML_SRC_DIR} to ${TARGET_DIR}"
+    rm -rf ${TARGET_DIR}
+    cp -r ${HTML_SRC_DIR} ${TARGET_DIR}
+elif [ -d ${HTML_SRC_DIR2} ]; then
+    echo "Copy ${HTML_SRC_DIR2} to ${TARGET_DIR}"
+    rm -rf ${TARGET_DIR}
+    cp -r ${HTML_SRC_DIR2} ${TARGET_DIR}
+fi
+
 git status
+
+echo "Done!"
